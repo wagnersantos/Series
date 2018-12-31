00061b042b9b38f1ec68c9a871a3d30bf13ed13c
wagnersantos <wagtvc@gmail.com>
2018-12-31 16:55:50 -0200

    adcionando metodo getSeriesById em services SeriesData

--- src/services/seriesData/SeriesData.js
+++ src/services/seriesData/SeriesData.js
@@ -2,6 +2,9 @@ const SeriesData = {
 	getSeriesByName(props){
 		return fetch(`http://api.tvmaze.com/search/shows?q=${props}`)
 	},
+	getSeriesById(props){
+		return fetch(`http://api.tvmaze.com/shows/${props.match.params.id}?embed=episodes`)
+	}
 }
 
 export default SeriesData;
