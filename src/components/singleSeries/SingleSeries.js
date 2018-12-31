4f7a068ea6fa4e2980d37f056421c0a0a1dbeced
wagnersantos <wagtvc@gmail.com>
2018-12-31 17:28:30 -0200

    adicionando validação e loader no componente SingleSeries

--- src/components/singleSeries/SingleSeries.js
+++ src/components/singleSeries/SingleSeries.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SeriesData from '../../services/seriesData/SeriesData';
+import Loader from '../../components/loader/Loader';
 
 class SingleSeries extends Component {
 	constructor(props){
@@ -18,23 +19,26 @@ class SingleSeries extends Component {
 		
 		return (
 			<div>
-			{
-				show !== null  
-				&& 
-				<div>
-					<p>{show.name}</p>
-					<p>Lançamento - {new Date(show.premiered).toLocaleDateString()}</p>
-					<p>Pontuação - {show.rating.average}</p>
-					<p>Episódios - {show._embedded.episodes.length}</p>
-					<p>Temporadas - {show._embedded.episodes[show._embedded.episodes.length-1]
-						.season}</p>
-					<p>
-						<img alt='show' src={show.image.medium} />
-					</p>
-					<p dangerouslySetInnerHTML={{__html: 
-        				show.summary}}></p>
-				</div>
-			}
+				{
+					show === null && <Loader />
+				}
+				{
+					show !== null  
+					&& 
+					<div>
+						<p>{show.name}</p>
+						<p>Lançamento - {new Date(show.premiered).toLocaleDateString()}</p>
+						<p>Pontuação - {show.rating.average}</p>
+						<p>Episódios - {show._embedded.episodes.length}</p>
+						<p>Temporadas - {show._embedded.episodes[show._embedded.episodes.length-1]
+							.season}</p>
+						<p>
+							<img alt='show' src={show.image.medium} />
+						</p>
+						<p dangerouslySetInnerHTML={{__html: 
+	        				show.summary}}></p>
+					</div>
+				}
 			</div>			
 		)
 	}
