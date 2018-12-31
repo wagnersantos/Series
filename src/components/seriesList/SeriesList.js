fc92242c8f88f62cace9f083ec5657de03fdfcea
wagnersantos <wagtvc@gmail.com>
2018-12-31 17:08:50 -0200

    adicionando links a página de visualização de series

--- src/components/seriesList/SeriesList.js
+++ src/components/seriesList/SeriesList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './index.css';
+import {Link} from 'react-router-dom';
+
+const SeriesListItem = ({series}) => (
+	<Link to={`/series/${series.show.id}`}>
+		<li>{series.show.name}</li>
+	</Link>
+);
 
 const SeriesList = (props) => {
 	return (
 		<div>
 			<ul className='series-list'>
 				{props.list.map(series => (
-					<li key={series.show.id}>{series.show.name}</li>
+					<SeriesListItem series={series} key={series.show.id}/>
 				))}
 			</ul>
 		</div>
