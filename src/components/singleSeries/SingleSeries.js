edb8d31dc6584581cb257d82a6781d176c53323a
wagnersantos <wagtvc@gmail.com>
2018-12-31 16:47:42 -0200

    adcionando construtor e setando estaod no componenete SingleSeries

--- src/components/singleSeries/SingleSeries.js
+++ src/components/singleSeries/SingleSeries.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 
 class SingleSeries extends Component {
+	constructor(props){
+		super(props);
+		this.state ={
+			show: null
+		}
+	}
 	render(){
 		return (
 			
