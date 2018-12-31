ff3ed178b075c5bf7fde03811294164dccf64844
wagnersantos <wagtvc@gmail.com>
2018-12-31 18:42:16 -0200

    adicionando componente NotFound

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Series from '../../components/series/Series';
 import SingleSeries from '../../components/singleSeries/SingleSeries';
+import NotFound from '../../components/notFound/NotFound';
 
 const Main = (props) => (
 	<Switch>
 		<Route exact path='/' component={Series} />	
 		<Route path='/series/:id' component={SingleSeries} />	
+		<Route component={NotFound}/>
 	</Switch>
 )
 
---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+		<div>
+			<p>Página não encontrada</p>
+		</div>
+)
+
+export default NotFound;
