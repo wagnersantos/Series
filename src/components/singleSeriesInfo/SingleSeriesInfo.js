3cf45ac8ee7b0bc18356eddf0457105c08b14ee4
wagnersantos <wagtvc@gmail.com>
2018-12-31 17:42:54 -0200

    adicionando link para voltar em SingleSeriesInfo

--- src/components/singleSeriesInfo/SingleSeriesInfo.js
+++ src/components/singleSeriesInfo/SingleSeriesInfo.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Loader from '../../components/loader/Loader';
+import {Link} from 'react-router-dom';
+
+const ButtonBack = ({series}) => (
+	<Link to={'/'}>
+		<div>
+			<p>Voltar</p>
+		</div>
+	</Link>
+);
 
 const SingleSeriesInfo = ({show}) => {
 	return (
@@ -11,17 +20,20 @@ const SingleSeriesInfo = ({show}) => {
 				show !== null  
 				&& 
 				<div>
-					<p>{show.name}</p>
-					<p>Lançamento - {new Date(show.premiered).toLocaleDateString()}</p>
-					<p>Pontuação - {show.rating.average}</p>
-					<p>Episódios - {show._embedded.episodes.length}</p>
-					<p>Temporadas - {show._embedded.episodes[show._embedded.episodes.length-1]
-						.season}</p>
-					<p>
-						<img alt='show' src={show.image.medium} />
-					</p>
-					<p dangerouslySetInnerHTML={{__html: 
-        				show.summary}}></p>
+					<div>
+						<p>{show.name}</p>
+						<p>Lançamento - {new Date(show.premiered).toLocaleDateString()}</p>
+						<p>Pontuação - {show.rating.average}</p>
+						<p>Episódios - {show._embedded.episodes.length}</p>
+						<p>Temporadas - {show._embedded.episodes[show._embedded.episodes.length-1]
+							.season}</p>
+						<p>
+							<img alt='show' src={show.image.medium} />
+						</p>
+						<p dangerouslySetInnerHTML={{__html: 
+	        				show.summary}}></p>
+					</div>
+					<ButtonBack />
 				</div>
 			}
 		</div>			
