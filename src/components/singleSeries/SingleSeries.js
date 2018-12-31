b493a15d21a764d425176cb201f6b4b2518300e8
wagnersantos <wagtvc@gmail.com>
2018-12-31 16:59:31 -0200

    trabalhando resposta em api em SingleSeries

--- src/components/singleSeries/SingleSeries.js
+++ src/components/singleSeries/SingleSeries.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SeriesData from '../../services/seriesData/SeriesData';
 
 class SingleSeries extends Component {
 	constructor(props){
@@ -8,7 +9,9 @@ class SingleSeries extends Component {
 		}
 	}
 	componentDidMount(){
-		
+		SeriesData.getSeriesById(this.props)
+      		.then(response => response.json())
+      		.then(json => this.setState({show: json}));
 	}
 	render(){
 		return (
