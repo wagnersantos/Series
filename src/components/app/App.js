a7b7d03595fffaf72c5041c1a698922d681c78ca
wagnersantos <wagtvc@gmail.com>
2018-12-31 12:06:11 -0200

    adicionado componente Intro

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Intro from '../../components/intro/Intro';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className='App-title'>Séries</h1>
         </header>
+        <Intro message ='Informações sobre séries'/>
       </div>
     );
   }
