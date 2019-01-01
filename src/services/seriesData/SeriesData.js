20ce3c7dcca38cc597e3acf53071bb2e3974b941
wagnersantos <wagtvc@gmail.com>
2019-01-01 00:41:23 -0200

    corrigindo erro de mixed content no componente SerieData

--- src/services/seriesData/SeriesData.js
+++ src/services/seriesData/SeriesData.js
@@ -1,9 +1,9 @@
 const SeriesData = {
 	getSeriesByName(props){
-		return fetch(`http://api.tvmaze.com/search/shows?q=${props}`)
+		return fetch(`https://api.tvmaze.com/search/shows?q=${props}`)
 	},
 	getSeriesById(props){
-		return fetch(`http://api.tvmaze.com/shows/${props.match.params.id}?embed=episodes`)
+		return fetch(`https://api.tvmaze.com/shows/${props.match.params.id}?embed=episodes`)
 	}
 }
 
